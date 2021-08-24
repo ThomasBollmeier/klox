9a51fc12b97172d9de0a0b91d1a923986161ae09
Thomas Bollmeier <developer@thomas-bollmeier.de>
2021-08-24 22:10:18 +0200

    Scanner development pt. 1

--- src/main/kotlin/de/tbollmeier/klox/Scanner.kt
+++ src/main/kotlin/de/tbollmeier/klox/Scanner.kt
@@ -2,8 +2,28 @@ package de.tbollmeier.klox
 
 class Scanner(private val source: String) {
 
+    private var current = 0
+    private var start = 0
+    private var line = 1
+
     fun scanTokens(): List<Token> {
-        return emptyList()
+
+        val ret = mutableListOf<Token>()
+
+        while (!isAtEnd()) {
+            start = current
+            val token: Token = scanToken()
+            ret.add(token)
+        }
+
+        ret.add(Token(TokenType.EOF, "", line))
+
+        return ret
     }
 
+    private fun scanToken(): Token {
+        TODO()
+    }
+
+    private fun isAtEnd() = current >= source.length
 }
--- src/main/kotlin/de/tbollmeier/klox/Token.kt
+++ src/main/kotlin/de/tbollmeier/klox/Token.kt
@@ -1,4 +1,12 @@
 package de.tbollmeier.klox
 
-class Token {
+class Token(
+    val tokenType: TokenType,
+    val lexeme: String,
+    val line: Int,
+    val literal: Any? = null
+    ) {
+
+    override fun toString() = "$tokenType $lexeme $literal"
+
 }
